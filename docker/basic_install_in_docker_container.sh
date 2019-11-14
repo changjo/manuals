c642dbb6e266cd39d6c78902e6b301265db2ec3b
changjo <wowchangjo@gmail.com>
2019-11-14 16:00:35 +0900

    Updated

--- docker/basic_install_in_docker_container.sh
+++ docker/basic_install_in_docker_container.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-apt update && apt upgrade -y
+apt update
 apt install -y apt-utils dialog
 apt install -y openssh-server
-apt install -y sudo vim nano net-tools man
+apt install -y vim nano
 apt install -y locales
 
 sed -i 's/PermitRootLogin/#PermitRootLogin/g' /etc/ssh/sshd_config
@@ -11,8 +11,8 @@ echo -e "PermitRootLogin yes" | tee -a /etc/ssh/sshd_config
 
 locale-gen en_US.UTF-8
 update-locale
-echo "LC_ALL=en_US.UTF-8" | sudo tee -a /etc/default/locale
-echo "LANGUAGE=en_US:en" | sudo tee -a /etc/default/locale
+echo "LC_ALL=en_US.UTF-8" | tee -a /etc/default/locale
+echo "LANGUAGE=en_US:en" | tee -a /etc/default/locale
 
 service ssh start
 
