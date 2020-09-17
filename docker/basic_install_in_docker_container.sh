a03384c90b3db38f37f5cc96be217ecc2ff8e026
Chang Jo Kim <wowchangjo@gmail.com>
2020-09-17 14:57:42 +0900

    Added changing time zone

--- docker/basic_install_in_docker_container.sh
+++ docker/basic_install_in_docker_container.sh
@@ -1,5 +1,8 @@
 #/usr/bin/env bash
 
+export DEBIAN_FRONTEND=noninteractive
+export TZ="Asia/Seoul"
+
 apt update && apt install -y \
   apt-utils \
   dialog \
@@ -7,7 +10,8 @@ apt update && apt install -y \
   vim \
   nano \
   tmux \
-  locales
+  locales \
+  tzdata
 
 sed -i 's/PermitRootLogin/#PermitRootLogin/g' /etc/ssh/sshd_config
 echo "PermitRootLogin yes" | tee -a /etc/ssh/sshd_config
@@ -16,5 +20,8 @@ locale-gen en_US.UTF-8
 update-locale
 echo "LC_ALL=en_US.UTF-8" | tee -a /etc/default/locale
 echo "LANGUAGE=en_US:en" | tee -a /etc/default/locale
+
+ln -fs /usr/share/zoneinfo/$TZ /etc/localtime
+dpkg-reconfigure -f noninterative tzdata
 
 service ssh start
